dbf05495a4d7b34ca2c88c6c82afa11b559c2dcb
Taylor Vann <brian.t.vann@gmail.com>
2025-04-05 15:58:21 -0700

    update events and exampels (#15)

--- dist/api/hx.js
+++ dist/api/hx.js
@@ -1,29 +1,50 @@
 import { dispatchHxEvent, dispatchHxFromForm } from "../hx-event/mod.js";
 const eventNames = [
+    "animationstart",
+    "animationend",
+    "animationcancel",
+    "copy",
+    "paste",
+    "cut",
+    "fullscreenchange",
+    "fullscreenerror",
+    "gotpointercapture",
+    "lostpointercapture",
+    "transitionstart",
+    "transitionrun",
+    "transitionend",
     "change",
     "click",
     "dblclick",
     "input",
     "keydown",
+    "keyup",
+    "focusin",
+    "focusout",
+    "blur",
+    "scroll",
+    "scrollend",
     "pointerover",
+    "pointerdown",
     "pointerup",
     "submit",
+    "reset",
+    "wheel",
 ];
+function isFormEvent(name) {
+    return "submit" === name || "reset" === name;
+}
 class Hx {
     connect(el) {
         // interactions
         for (let name of eventNames) {
-            let dispatch = "submit" === name || "reset" === name
-                ? dispatchHxFromForm
-                : dispatchHxEvent;
+            let dispatch = isFormEvent(name) ? dispatchHxFromForm : dispatchHxEvent;
             el.addEventListener(name, dispatch);
         }
     }
     disconnect(el) {
         for (let name of eventNames) {
-            let dispatch = "submit" === name || "reset" === name
-                ? dispatchHxFromForm
-                : dispatchHxEvent;
+            let dispatch = isFormEvent(name) ? dispatchHxFromForm : dispatchHxEvent;
             el.removeEventListener(name, dispatch);
         }
     }
